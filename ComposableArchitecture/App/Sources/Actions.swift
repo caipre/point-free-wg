9bdabe0ceefb41c4822f3c453746c11aabb07fcd
Joe Levin <joe.levin@gmail.com>
2021-01-04 12:23:58 -0500

    Add first pass of actions

---
+++ ComposableArchitecture/App/Sources/Actions.swift
@@ -0,0 +1,29 @@
+enum AppAction {
+    case home(HomeAction)
+    case recents(RecentsAction)
+    case favorites(FavoritesAction)
+    case word(WordAction)
+}
+
+enum HomeAction {
+    case didTapLanguage
+    case didTapTodaysWord
+    case didTapRecentlyViewed
+    case didTapFavorites
+    case didTapSearch
+}
+
+enum RecentsAction {
+    case didTapWord
+    case didTapClearAll
+}
+
+enum FavoritesAction {
+    case didTapWord
+    case didTapRemoveWord
+}
+
+enum WordAction {
+    case didTapFavorite
+    case didTapWord
+}
