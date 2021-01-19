1da3e8e15ddcb6a4c37c8594b959eeb11d07c037
Joe Levin <joe.levin@gmail.com>
2021-01-19 13:00:32 -0500

    Add favorites view first pass

---
+++ ComposableArchitecture/App/Sources/Favorites/FavoritesView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct FavoritesView: View {
+  @State var words: [Word] = ["test", "test"]
+
+  var body: some View {
+    List(words, id: \.self) { element in
+      Text(element)
+    }
+  }
+}
