17932d0df6dd9496775ed8884dfb4d557665b34c
Joe Levin <joe.levin@gmail.com>
2021-01-19 12:49:46 -0500

    Add first pass recents view

--- ComposableArchitecture/App/Sources/Home/HomeView.swift
+++ ComposableArchitecture/App/Sources/Home/HomeView.swift
@@ -5,18 +5,20 @@ struct HomeView: View {
   @State private var searchQuery: String = ""
 
   var body: some View {
-    VStack {
-      Picker("Language picker", selection: $selection) {
-        ForEach(Language.allCases, id: \.self) { element in
-          Text(element.rawValue)
-        }
-      }.pickerStyle(MenuPickerStyle())
-        .background(Color.gray)
-      TextField("Search", text: $searchQuery)
-        .background(Color.gray)
-      Button("Today's word".localizedCapitalized) {}
-      Button("Recently viewed".localizedCapitalized) {}
-      Button("Favorites".localizedCapitalized) {}
-    }.padding(EdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16))
+    NavigationView {
+      VStack {
+        Picker("Language picker", selection: $selection) {
+          ForEach(Language.allCases, id: \.self) { element in
+            Text(element.rawValue)
+          }
+        }.pickerStyle(MenuPickerStyle())
+          .background(Color.gray)
+        TextField("Search", text: $searchQuery)
+          .background(Color.gray)
+        NavigationLink("today's word".localizedCapitalized, destination: EmptyView())
+        NavigationLink("Recently viewed".localizedCapitalized, destination: RecentsView())
+        NavigationLink("Favorites".localizedCapitalized, destination: EmptyView())
+      }.padding(EdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16))
+    }
   }
 }
---
+++ ComposableArchitecture/App/Sources/Recents/RecentsView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct RecentsView: View {
+  @State var words: [Word] = ["test", "test"]
+
+  var body: some View {
+    List(words, id: \.self) { element in
+      Text(element)
+    }
+  }
+}
