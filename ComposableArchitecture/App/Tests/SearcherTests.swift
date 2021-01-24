d555e1a7f857ab82280a068895dc1d4f8663ab8c
Nick Platt <platt.nicholas@gmail.com>
2021-01-23 21:49:22 -0500

    test: initial searcher tests

---
+++ ComposableArchitecture/App/Tests/SearcherTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+
+@testable import App
+
+class SearcherTests: XCTestCase {
+  var sut: Searcher!
+  override func setUp() {
+  }
+
+  func testThatItMatchesFuzzily() {
+    sut = SearcherImpl(index: ["one", "two", "three"])
+    let actual = sut.search(query: "o").map(\.word)
+    let expected: [Word] = ["one", "two"]
+    XCTAssertEqual(actual, expected)
+  }
+}
