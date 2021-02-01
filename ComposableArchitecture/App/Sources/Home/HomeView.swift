a013a6c346dae875d293b7260912c6b8b4b75d1d
Joe Levin <joe.levin@gmail.com>
2021-02-01 16:41:42 -0500

    Implement one action: changing the language

--- ComposableArchitecture/App/Sources/Home/HomeView.swift
+++ ComposableArchitecture/App/Sources/Home/HomeView.swift
@@ -2,19 +2,21 @@ import SwiftUI
 
 struct HomeView: View {
   @ObservedObject var store: Store<AppState, AppAction>
-
-  //  @State private var selection: Language = .en
+  @State private var selection: Language = .en
   //  @State private var searchQuery: String = ""
 
   var body: some View {
     NavigationView {
       VStack {
-        Picker("Language picker", selection: $store.value.currentLanguage) {
+        Picker("Language picker", selection: $selection) {
           ForEach(Language.allCases, id: \.self) { element in
             Text(element.rawValue)
           }
         }.pickerStyle(MenuPickerStyle())
           .background(Color.gray)
+          .onChange(of: selection) { selection in
+            store.send(.home(.didTapLanguage(language: selection)))
+          }
         TextField("Search", text: $store.value.search.query)
           .background(Color.gray)
         NavigationLink("today's word".localizedCapitalized, destination: WordDefinitionView())
