4ac8080981a4c1d6fffabba682c8bd55d8a41d6c
Joe Levin <joe.levin@gmail.com>
2021-02-01 16:32:31 -0500

    Add barebones reducer

--- ComposableArchitecture/App/Sources/Home/HomeView.swift
+++ ComposableArchitecture/App/Sources/Home/HomeView.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 struct HomeView: View {
-  @ObservedObject var store: Store<AppState>
+  @ObservedObject var store: Store<AppState, AppAction>
 
   //  @State private var selection: Language = .en
   //  @State private var searchQuery: String = ""
