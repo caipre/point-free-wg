e0387c7540534d257e10a7a8cca2c666609523aa
Joe Levin <joe.levin@gmail.com>
2021-03-08 14:08:14 -0500

    Add home reducer

--- MotDuJour/Features/Home/Sources/Home.swift
+++ MotDuJour/Features/Home/Sources/Home.swift
@@ -1,7 +1,7 @@
 import Models
 
 public struct HomeState {
-    private let language: Language
+    public var language: Language
 
     public init(
         language: Language
@@ -17,4 +17,21 @@ public enum HomeAction {
     case didTapRecentlyViewed
     case didTapFavorites
     case didTapSearch(query: String)
+}
+
+public enum HomeReducer {
+    public static func reducer(value: inout HomeState, action: HomeAction) {
+        switch action {
+        case .didTapLanguage(let language):
+            value.language = language
+        case .didTapTodaysWord:
+            print("didTapTodaysWord")
+        case .didTapRecentlyViewed:
+            print("didTapRecentlyViewed")
+        case .didTapFavorites:
+            print("didTapFavorites")
+        case .didTapSearch(let query):
+            print(query)
+        }
+    }
 }
