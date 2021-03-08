f8d6ed0f688d41088dc70ea53b9c19430b1b0071
Joe Levin <joe.levin@gmail.com>
2021-03-08 11:45:12 -0500

    Use module to load resources in test target because test targets cannot access the main bundle

--- MotDuJour/Languages/Sources/Loaders.swift
+++ MotDuJour/Languages/Sources/Loaders.swift
@@ -4,7 +4,7 @@ public class Loaders {
     private static let decoder = JSONDecoder()
 
     public static func load<T: Decodable>(type: T.Type, resource: String) -> T {
-        let url = Bundle.main.url(forResource: resource, withExtension: "lzfse")!
+        let url = Bundle.module.url(forResource: resource, withExtension: "lzfse")!
         let dataz = try! Data(contentsOf: url)
         let data = try! (dataz as NSData).decompressed(using: .lzfse)
 
