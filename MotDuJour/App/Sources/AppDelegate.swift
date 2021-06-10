bee5f83b457256ed8df63674602a99cb26d52f43
Erik Price <erikp@spotify.com>
2021-06-10 14:59:26 -0400

    Naive state persistence on termination

--- MotDuJour/App/Sources/AppDelegate.swift
+++ MotDuJour/App/Sources/AppDelegate.swift
@@ -1,3 +1,4 @@
+
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation; either version 2 of the License, or
@@ -36,5 +37,16 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
             name: "Default Configuration",
             sessionRole: connectingSceneSession.role
         )
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+//       This is what we want to say:
+//       store.send(.willTerminate)
+
+        // But for now, we do it the naive way:
+        let data = try! JSONEncoder().encode(self.store.value)
+        let documentsUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let favoritePrimesUrl = documentsUrl.appendingPathComponent("app-state.json")
+        try! data.write(to: favoritePrimesUrl)
     }
 }
